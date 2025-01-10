a2273c6d258b89f266866a833f9517bf543c3eb6
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2025-01-10 23:34:06 +0700

    refactor ~01

--- src/GLSLUniform.svelte
+++ src/GLSLUniform.svelte
@@ -1,61 +1,76 @@
 <script lang="ts">
-  import p5 from "p5";
+  import      p5                   from "p5";
   import          "beercss"       ;
   import "material-dynamic-colors";
-  import type { GLSLUniformValue, GLSLUniforms,TextureForShader } from "./types";
-import {onMount} from "svelte";
-  import { splitArrayIntoGroups } from "./common";
-
+  import type { GLSLUniformValue } from "./types";
+  import type { GLSLUniforms     } from "./types";
+  import type { TextureForShader } from "./types";
+  import {               onMount } from   "svelte";
+  import { splitArrayIntoGroups  } from "./common";
   export let canvasInstance: p5 = null!;
   export let uniforms:                   GLSLUniforms  =  [  ];
   export let onUpdate: (updatedUniforms: GLSLUniforms) => void;
-
-  import { texturesNoise } from "./global";
-import { texturesBayer } from "./global";
-import { texturesPalette } from "./global";
-import { texturesPencil } from "./global";
-import { texturesASCII } from "./global";
-import { texturesTiled } from "./global";
-import { texturesShaderToy } from "./global";
-
-
-
-
- 
-
+  import { texturesNoise     } from "./global";
+  import { texturesBayer     } from "./global";
+  import { texturesPalette   } from "./global";
+  import { texturesPencil    } from "./global";
+  import { texturesASCII     } from "./global";
+  import { texturesTiled     } from "./global";
+  import { texturesShaderToy } from "./global";
 
 
 
   // Update uniforms when a value is changed
+  // Update uniforms when a value is changed
   const updateUniform = (
-    key: number,
-    index: number | null,
-    newValue: number | boolean | string,
-  ): void => {
+    key     : number
+  ,    index: number | null
+  , newValue: number | boolean | string
+  ,
+                        ): void => {
+    // const updatedUniforms: GLSLUniforms = { ...uniforms };
     // const updatedUniforms: GLSLUniforms = { ...uniforms };
     if (Array.isArray(uniforms[key].thisUniformDefaultValue)) {
-      if (index !== null)
-        (uniforms[key].thisUniformDefaultValue as number[])[index] = newValue as number;
-    } else {
-      uniforms[key].thisUniformDefaultValue = newValue;
+    if (index !== null)                                       {
+      (
+        uniforms[key].thisUniformDefaultValue as number[])[index] = newValue as number;
+      }
+    } else                                                                  {
+        uniforms[key].thisUniformDefaultValue                     = newValue          ;
     }
-    if (canvasInstance && uniforms[key].thisUniformType === "sampler2D") {
-      uniforms[key].thisUniformSampler2DImg = canvasInstance.loadImage(uniforms[key].thisUniformDefaultValue as string);
+    if (                                         canvasInstance
+    &&  uniforms[key].thisUniformType     ===                  "sampler2D") {
+        uniforms[key].thisUniformSampler2DImg =  canvasInstance.loadImage (
+        uniforms[key].thisUniformDefaultValue as string                   );
     }
-    onUpdate?.(uniforms); // Trigger callback
+    // Trigger callback
+    // Trigger callback
+    onUpdate?.(uniforms);
   };
 
 
 
-  let interval:NodeJS.Timeout;
+  let interval: NodeJS.Timeout = null!;
 </script>
 
 <div>
-  {#each uniforms as {thisUniformName, thisUniformNameJustForDisplay, thisUniformType, thisUniformDefaultValue, thisUniformSampler2DImg, thisUniformSampler2DEle, }, ii (thisUniformName)}
+  {#each uniforms as { thisUniformName
+   ,                   thisUniformNameJustForDisplay
+   ,                   thisUniformType
+   ,                   thisUniformDefaultValue
+   ,                   thisUniformSampler2DImg
+   ,                   thisUniformSampler2DEle
+   ,                 }
+   ,              ii ( thisUniformName )
+  }
     <div>
       <strong>{thisUniformNameJustForDisplay ?? thisUniformName} ({thisUniformType}):</strong>
       <!-- Render inputs based on type -->
-      {#if ((thisUniformType ?? "").startsWith("vec") || (thisUniformType ?? "").startsWith("ivec") || (thisUniformType ?? "").startsWith("uvec")) && thisUniformDefaultValue instanceof Array}
+      {#if ((thisUniformType ?? "").startsWith( "vec")
+        ||  (thisUniformType ?? "").startsWith("ivec")
+        ||  (thisUniformType ?? "").startsWith("uvec"))
+        &&   thisUniformDefaultValue instanceof Array
+      }
         <table class="center-align">
           <thead>
             <tr>
@@ -77,19 +92,20 @@ import { texturesShaderToy } from "./global";
                 <td class="s2 field small border">
                   <input
                     type="number"
-                    step="0.01"
-                    value={v}
-                    on:input={(e) =>
-                      updateUniform(ii, i, parseFloat(e.currentTarget.value))}
+                    step="0.0100"
+                       value={       v                                                                 }
+                    on:input={async (e) => { updateUniform(ii, i, parseFloat(e.currentTarget.value)); }}
                   />
                 </td>
               {/each}
             </tr>
           </tbody>
         </table>
-      {:else if (thisUniformType ?? "").startsWith("mat") && thisUniformDefaultValue instanceof Array}
-        {#if thisUniformDefaultValue.length === 4}
-          {#each splitArrayIntoGroups(thisUniformDefaultValue,2) as r, ri }
+      {:else if (thisUniformType ?? ""). startsWith("mat")
+             &&  thisUniformDefaultValue instanceof Array
+      }
+        {#if                          thisUniformDefaultValue.length === 4           }
+          {#each splitArrayIntoGroups(thisUniformDefaultValue         ,  2) as r, ri }
             <table class="center-align">
               <thead>
                 <tr>
@@ -109,10 +125,9 @@ import { texturesShaderToy } from "./global";
                     <td class="s2 field small border">
                       <input
                         type="number"
-                        step="0.01"
-                        value={c}
-                        on:input={(e) =>
-                        updateUniform(ii, ri * 2 + ci, parseFloat(e.currentTarget.value))}
+                        step="0.0100"
+                           value={       c                                                                           }
+                        on:input={async (e) => { updateUniform(ii, ri * 2 + ci, parseFloat(e.currentTarget.value)); }}
                       />
                     </td>
                   {/each}
@@ -121,8 +136,8 @@ import { texturesShaderToy } from "./global";
             </table>
           {/each}
         {/if}
-        {#if thisUniformDefaultValue.length === 9}
-          {#each splitArrayIntoGroups(thisUniformDefaultValue,3) as r, ri }
+        {#if                          thisUniformDefaultValue.length === 9           }
+          {#each splitArrayIntoGroups(thisUniformDefaultValue         ,  3) as r, ri }
             <table class="center-align">
               <thead>
                 <tr>
@@ -149,10 +164,9 @@ import { texturesShaderToy } from "./global";
                     <td class="s2 field small border">
                       <input
                         type="number"
-                        step="0.01"
-                        value={c}
-                        on:input={(e) =>
-                        updateUniform(ii, ri * 3 + ci, parseFloat(e.currentTarget.value))}
+                        step="0.0100"
+                           value={       c                                                                           }
+                        on:input={async (e) => { updateUniform(ii, ri * 3 + ci, parseFloat(e.currentTarget.value)); }}
                       />
                     </td>
                   {/each}
@@ -161,8 +175,8 @@ import { texturesShaderToy } from "./global";
             </table>
           {/each}
         {/if}
-        {#if thisUniformDefaultValue.length === 16}
-          {#each splitArrayIntoGroups(thisUniformDefaultValue,4) as r, ri }
+        {#if                          thisUniformDefaultValue.length === 16          }
+          {#each splitArrayIntoGroups(thisUniformDefaultValue         ,  4) as r, ri }
             <table>
               <thead>
                 <tr>
@@ -198,10 +212,9 @@ import { texturesShaderToy } from "./global";
                     <td class="s2 field small border">
                       <input
                         type="number"
-                        step="0.01"
-                        value={c}
-                        on:input={(e) =>
-                        updateUniform(ii, ri * 4 + ci, parseFloat(e.currentTarget.value))}
+                        step="0.0100"
+                           value={       c                                                                           }
+                        on:input={async (e) => { updateUniform(ii, ri * 4 + ci, parseFloat(e.currentTarget.value)); }}
                       />
                     </td>
                   {/each}
@@ -210,33 +223,37 @@ import { texturesShaderToy } from "./global";
             </table>
           {/each}
         {/if}
-      {:else if (thisUniformType ?? "") === "bool" && typeof thisUniformDefaultValue === "boolean"}
-        <label class="switch">
+      {:else if       (thisUniformType  ??  "") === "bool   ".trim()
+             && typeof thisUniformDefaultValue  === "boolean"       }
+        <label  class="switch">
           <input
             type="checkbox"
-            checked={thisUniformDefaultValue}
-            on:change={(e) => updateUniform(ii, null, e.currentTarget.checked)}
+                  checked={                                       thisUniformDefaultValue    }
+                on:change={async (e) => { updateUniform(ii, null, e.currentTarget.checked); }}
           />
           <span>
             <i></i>
             <i></i>
           </span>
         </label>
-      {:else if (thisUniformType ?? "") === "float" || (thisUniformType ?? "") === "int" || (thisUniformType ?? "") === "uint"}
-        <div class="grid">
+      {:else if (thisUniformType ?? "") === "float"
+             || (thisUniformType ?? "") ===        "int"
+             || (thisUniformType ?? "") ===             "uint"}
+        <div   class="grid                 ">
           <div class="s2 field small border">
             <input
               type="number"
-              step="1"
-              value={thisUniformDefaultValue}
+              step="1.0000"
+                 value={thisUniformDefaultValue}
               on:input={(e) =>
-                updateUniform(
-                  ii,
+              updateUniform  (
+                   ii ,
                   null,
                   (thisUniformType ?? "") === "int" || (thisUniformType ?? "") === "uint"
                     ? parseInt  (e.currentTarget.value)
-                    : parseFloat(e.currentTarget.value),
-                )}
+                    : parseFloat(e.currentTarget.value)
+                             )
+                       }
             />
           </div>
           <div      class="center center-align">
@@ -255,35 +272,43 @@ import { texturesShaderToy } from "./global";
             on:mouseleave={async (e) => { clearInterval(interval); }}><i class="fa-solid fa-caret-right"></i></button >
           </div>
         </div>
-      {:else if ((thisUniformType ?? "") === "sampler2D" || (thisUniformType ?? "") === "sampler3D") && typeof thisUniformDefaultValue === "string"}
+      {:else if ((thisUniformType ?? "") === "sampler2D"
+             ||  (thisUniformType ?? "") === "sampler3D")
+             &&                               typeof     thisUniformDefaultValue === "string"}
         <!-- <span>sampler2D (not editable)</span> -->
-        <img class="small-width small-height" src={thisUniformDefaultValue} alt="" bind:this={thisUniformSampler2DEle}/>
+        <!-- <span>sampler2D (not editable)</span> -->
+        <img class="small-width small-height"       src={thisUniformDefaultValue}
+                                                    alt=""
+                                              bind:this={thisUniformSampler2DEle}/>
         <!-- {#if (thisUniformName ?? "").startsWith("noise")}
-          
+        <!-- {#if (thisUniformName ?? "").startsWith("noise")}
         {:else if (thisUniformName ?? "").startsWith("bayer")}
         {:else if (thisUniformName ?? "").startsWith("palette")}
         {/if} -->
         <div class="field label suffix round border">
           <select on:change={async (e) => {
-            let chosen = e.currentTarget.options[e.currentTarget.selectedIndex].value;
+            let chosen =            e.currentTarget.options[e.currentTarget.selectedIndex].value;
             if (chosen === "none") {
               uniforms[ii].thisUniformDefaultValue = null;
               uniforms[ii].thisUniformSampler2DImg = null;
               return;
             }
-            if (thisUniformSampler2DEle)
-            thisUniformSampler2DEle.src = chosen;
+            if (thisUniformSampler2DEle)  {
+                thisUniformSampler2DEle.src = chosen;
+            }
             updateUniform(ii, null, chosen);
           }}>
-            {#each ((thisUniformName ?? "").startsWith("noise") ? $texturesNoise : 
-                    (thisUniformName ?? "").startsWith("bayer") ? $texturesBayer : 
-                    (thisUniformName ?? "").startsWith("palette") ? $texturesPalette : 
-                    (thisUniformName ?? "").startsWith("pencil") ? $texturesPencil :
-                    (thisUniformName ?? "").startsWith("ascii") ? $texturesASCII :
-                    (thisUniformName ?? "").startsWith("tiled") ? $texturesTiled :
+            {#each ((thisUniformName ?? "").startsWith("noise"    ) ? $texturesNoise     : 
+                    (thisUniformName ?? "").startsWith("bayer"    ) ? $texturesBayer     : 
+                    (thisUniformName ?? "").startsWith("palette"  ) ? $texturesPalette   : 
+                    (thisUniformName ?? "").startsWith("pencil"   ) ? $texturesPencil    :
+                    (thisUniformName ?? "").startsWith("ascii"    ) ? $texturesASCII     :
+                    (thisUniformName ?? "").startsWith("tiled"    ) ? $texturesTiled     :
                     (thisUniformName ?? "").startsWith("shaderToy") ? $texturesShaderToy :
-                    [{ name: "none", path: "none", }]) as textureForShader (textureForShader)}
-              <option value={textureForShader.path}>{textureForShader.name}</option>            
+                    [{ name: "none", path: "none", }]) as textureForShader
+                                                         (textureForShader)
+            }
+                   <option value={textureForShader.path}>{textureForShader.name}</option>
             {/each}
           </select>
           <!-- svelte-ignore a11y-label-has-associated-control -->
@@ -299,6 +324,8 @@ import { texturesShaderToy } from "./global";
     <div class="large-space"></div>
   {/each}
 </div>
+
+
 
 
 

58ddd0d15af5e43531b8d3ee4ac51c6d2071f424
pham_tuan_anh <pham_tuan_anh@optimizer.co.jp>
2025-01-16 17:10:22 +0700

    implement ~06

--- src/shadertoys/128.glsl
+++ src/shadertoys/128.glsl
@@ -14,106 +14,76 @@ uniform         vec4      mousePosition;
 ////////////////////////////
 ////////////////////////////
 
-const float SAMPLES = 10.; 
+uniform float   SAMPLES ; // 10.00
 
-float decay = 0.97; 
-float density = 0.15;
+uniform float   decay   ; // 00.97 
+uniform float   density ; // 00.15
 
-float threshold = 0.7;
+uniform float threshold ; // 00.70
 
 ////////////////////////////
 ////////////////////////////
 
 
-float hash( vec2 p ){ return fract(sin(dot(p, vec2(41, 289)))*45758.5453); }
-vec3 lOff(){ return vec3(0., 0., 0.); }
-	
+    float hash( vec2 p ) { return fract(sin(dot(p, vec2(41, 289))) * 45758.5453); }
+//  float hash( vec2 p ) { return fract(sin(dot(p, vec2(41, 289))) * 45758.5453); }
+    vec3  lOff(        ) { return vec3(0., 0., 0.); }
+//  vec3  lOff(        ) { return vec3(0., 0., 0.); }
+    
 
-vec3 rainbow_1(float x) {    
+vec3  rainbow_1(float x) {    
     return vec3(
-        (tanh(-10.*x + 2.) + 1.) * 0.5 + (tanh(10.*x - 7.) + 1.) * 0.5,
-        (tanh(-10.*x + 3.) + 1.) * 0.5,
-        (tanh(-10.*x + 6.) + 1.) * 0.5
+        (tanh(-10. * x + 2.) + 1.) * 0.5 + (tanh(10. * x - 7.) + 1.) * 0.5,
+        (tanh(-10. * x + 3.) + 1.) * 0.5,
+        (tanh(-10. * x + 6.) + 1.) * 0.5
     );
 }
-vec3 rainbow_2(float x) {
+vec3  rainbow_2(float x) {
     return vec3(
         1.,
-        (tanh(-10.*x + 6.) + 1.) * 0.5,
-        (tanh(-10.*x + 3.) + 1.) * 0.5
+        (tanh(-10. * x + 6.) + 1.) * 0.5,
+        (tanh(-10. * x + 3.) + 1.) * 0.5
     );
 }
-vec3 rainbow_3(float x) {
+vec3  rainbow_3(float x) {
     return vec3(
-        (tanh(-10.*x + 3.) + 1.) * 0.5,
-        (tanh(-10.*x + 6.) + 1.) * 0.5,
+        (tanh(-10. * x + 3.) + 1.) * 0.5,
+        (tanh(-10. * x + 6.) + 1.) * 0.5,
         1.
     );
 }
 
 
 
-vec3 shine(vec2 original_uv, vec2 tuv) {
-    vec2 dTuv = tuv*density/SAMPLES;
-    vec4 currentColor;
-    vec3 result = vec3(0.);
-    
-    float weight = 0.05;
-    vec2 uv = original_uv;
-    uv += dTuv*(hash(uv.xy)*2. - 1.);
-    
-    for(float i=0.; i < SAMPLES; i++){
-        uv -= dTuv;
-        currentColor = texture(iChannel0, uv);
-        result += (
-            step(threshold, length(currentColor.rgb)/sqrt(3.)) *
-            currentColor.rgb * 
-            rainbow_2(i/SAMPLES) * 
-            weight
-        );
-        weight *= decay;
-    }
-    
+vec3 shine(vec2 original_uv     ,
+           vec2         tuv
+          )
+{
+    vec2 dTuv  =  tuv * density / SAMPLES; vec4 currentColor; vec3 result = vec3(0.f); float weight = 0.05f; vec2 uv = original_uv; uv += dTuv * (hash(uv.xy) * 2.f - 1.f); for (float i = 0.f; i < SAMPLES; i++) { uv -= dTuv; currentColor = texture(tex0, uv); result += (step(threshold, length(currentColor.rgb) / sqrt(3.f)) * currentColor.rgb * rainbow_2(i / SAMPLES) * weight); weight *= decay; }
     ////////////////////////////////
     ////////////////////////////////
-    
-    dTuv *= -1.;
-    weight = 0.05;
-    uv = original_uv;
-    uv += dTuv*(hash(uv.xy)*2. - 1.);
+         dTuv *= -1.f                    ;                                                   weight = 0.05f;      uv = original_uv; uv += dTuv * (hash(uv.xy) * 2.f - 1.f); for (float i = 0.f; i < SAMPLES; i++) { uv -= dTuv; currentColor = texture(tex0, uv); result += (step(threshold, length(currentColor.rgb) / sqrt(3.f)) * currentColor.rgb * rainbow_3(i / SAMPLES) * weight); weight *= decay; }
 
-    for(float i=0.; i < SAMPLES; i++){
-        uv -= dTuv;
-        currentColor = texture(iChannel0, uv);
-        result += (
-            step(threshold, length(currentColor.rgb)/sqrt(3.)) *
-            currentColor.rgb * 
-            rainbow_3(i/SAMPLES) * 
-            weight
-        );
-        weight *= decay;
-    }
-    
     return result;
+//  return result;
 }
 
 
-void mainImage( out vec4 fragColor, in vec2 fragCoord ){
-    vec2 uv = fragCoord.xy / iResolution.xy;
-    
-    vec2 tuv;
-    vec4 col = texture(iChannel0, uv.xy)*0.8;
-    
+void main() {
+    vec2 uv = vTexCoord; vec2 tuv; vec4 col = texture(tex0, uv.xy) * 0.8;
     float x = 0.5;
     float y = 0.5;
-    
-    tuv = vec2(x, y);
-    col.rgb += shine(uv, tuv);
-    
-    
-    tuv = vec2(x, -y);
-    col.rgb += shine(uv, tuv);
-    
+    tuv = vec2(x,  y); col.rgb += shine(uv, tuv);
+    tuv = vec2(x, -y); col.rgb += shine(uv, tuv);
     fragColor = sqrt(smoothstep(0., 1., col));
 }
+
+
+
+
+
+
+
+
+
 

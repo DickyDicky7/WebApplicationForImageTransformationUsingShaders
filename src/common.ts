ffae573241bf17a32de61e0fa3fcbdd4c391f958
21520147-PhamTuanAnh <21520147@gm.uit.edu.vn>
2025-01-21 00:24:58 +0700

    fix

--- src/common.ts
+++ src/common.ts
@@ -1993,7 +1993,13 @@ export const onUndoActionExecuted = async () => {
 };
 
 
-
+export const doHexToRgbNormalized = async (hex: string): Promise<{ r: number, g: number, b: number }> => {
+    hex = hex.replace(/^#/, '');
+    const r = parseInt(hex.substring(0, 2), 16) / 255;
+    const g = parseInt(hex.substring(2, 4), 16) / 255;
+    const b = parseInt(hex.substring(4, 6), 16) / 255;
+    return { r, g, b };
+};
 
 
 

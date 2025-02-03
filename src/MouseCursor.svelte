37174f0e0567431ec838bac7658fb33b95d688b3
pham_tuan_anh <pham_tuan_anh@optimizer.co.jp>
2025-02-03 10:38:35 +0700

    gay ~01

--- src/MouseCursor.svelte
+++ src/MouseCursor.svelte
@@ -1,9 +1,9 @@
-    <script>import { Cursor } from "svelte-cursor";</script>
-<!--<script>import { Cursor } from "svelte-cursor";</script>-->
+    <script>import { Cursor } from "svelte-cursor"; import magicWand from "./assets/magic-wand-magician-svgrepo-com.svg";</script>
+<!--<script>import { Cursor } from "svelte-cursor"; import magicWand from "./assets/magic-wand-magician-svgrepo-com.svg";</script>-->
 
 <!-- svelte-ignore element_invalid_self_closing_tag -->
 <!-- svelte-ignore element_invalid_self_closing_tag -->
-    <Cursor><div class="cursor" /></Cursor>
-<!--<Cursor><div class="cursor" /></Cursor>-->
+    <Cursor><div class="cursor" /><img src={magicWand} alt="magic-wand" style:width="80px" style:height="80px"></Cursor>
+<!--<Cursor><div class="cursor" /><img src={magicWand} alt="magic-wand" style:width="80px" style:height="80px"></Cursor>-->
 
-<style>.cursor { width: 20px; aspect-ratio: 1; background-color: white; border-radius: 50%; }</style>
+<style>/*.cursor { width: 20px; aspect-ratio: 1; background-color: white; border-radius: 50%; }*/</style>
